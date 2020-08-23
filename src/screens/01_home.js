656f458b8d2361004c2d46a09593231d8a04bf20
Ryan Dick <rdick@ualberta.ca>
2020-08-23 14:20:01 -0600

    added button and background for about section

--- src/screens/01_home.js
+++ src/screens/01_home.js
@@ -4,15 +4,31 @@ import "./01_home.css"
 const Home = (props) => {
     return (
         <div className="home-page">
-            <div className="home-banner">
-                <div className="home-banner-text-container">
-                    <h3> Let's Transform</h3>
-                    <h2>Your Buisness</h2>
-                    <h1>One Client At A Time</h1>
+            <div className="home-page1">
+                <div className="home-banner1">
+                    <div className="home-banner-text-container">
+                        <h3> Let's Transform</h3>
+                        <h2>Your Buisness</h2>
+                        <h1>One Client At A Time</h1>
+                        <button><h4>Find Out More -></h4></button>
+                    </div>
+                    <div className="home-picture-container">
+                        <img src="/home-banner.png" alt="banner-image"></img>
+
+                    </div>
                 </div>
-                <div className="home-picture-container">
-                    <img src="/home-banner.png" alt="banner-image"></img>
+                <div className="home-banner2">
+                    <div className="companies-heading">
+                        <h3>Some of Our Clients</h3>
+                    </div>
+                    <div className="companies-icons">
+
+                    </div>
                 </div>
+            </div>
+
+            <div className="about-banner">
+
             </div>
 
         </div>
